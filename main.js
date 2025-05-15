41e6a98b0d4bf3ad229f13bdd50a2f319a4538ec
carnalheart <carnalatrium@example.com>
2025-05-15 11:01:18 +0100

    commit

--- main.js
+++ main.js
@@ -43,14 +43,13 @@ for (const file of commandFiles) {
 }
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
-  console.log('Connected to MongoDB!');
-}).catch((err) => {
-  console.error('MongoDB connection error:', err);
-});
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('Connected to MongoDB!');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
 
 // Register Slash Commands (Multiple Guilds)
 client.once('ready', async () => {
